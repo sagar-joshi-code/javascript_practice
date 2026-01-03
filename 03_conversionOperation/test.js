3c574d53e16d2dba6f78937f937cff4deab2aa5e
Sagar Joshi <sagarjo637@gmail.com>
2026-01-03 11:56:25 +0000

    practice datatype conversion

---
+++ 03_conversionOperation/test.js
@@ -0,0 +1,52 @@
+// let score="sagar"
+ 
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valInNumber=Number(score)
+// console.log(typeof valInNumber);
+// console.log( valInNumber);
+
+
+
+// let a=20.123;
+// console.log(typeof a)  //in js there is not a float and all floating number are treated as number
+
+
+//number conversion
+
+//"33" =>number
+//"33aaa" =>NaN
+//true =>1 ; false=>0
+
+
+
+// let isLoggedIn="sagar"
+
+// let booleanisLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
+
+
+//boolean conversion
+
+//1 =>true ; 0=>false
+//""=>false
+//"sagar"=>true
+
+
+
+// string conversion
+
+let number=undefined
+let stringInnumber=String(number)
+console.log(stringInnumber);
+console.log(typeof stringInnumber);
+
+
+
+
+
+
+
+
+
