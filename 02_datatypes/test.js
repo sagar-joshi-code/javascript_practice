fa665e7a38030a1e5432a12d97ee456ec713bfd1
Sagar Joshi <sagarjo637@gmail.com>
2026-01-03 07:22:48 +0000

    practice datatypes

---
+++ 02_datatypes/test.js
@@ -0,0 +1,18 @@
+//datatypes
+
+let name="sagar"
+let age=20
+let isLoggedIn=true
+let value=null;
+
+//number
+// BigInt
+//string=""
+//boolean=true/false
+// undefined
+//null =representation of empty value (null is a object)
+
+
+console.log(typeof null)  //typeof  tells the type of a value or variable.
+
+
