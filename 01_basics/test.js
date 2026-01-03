a516fdd42ecb954d8934ae0d49a6e3b21232257e
Sagar Joshi <sagarjo637@gmail.com>
2026-01-03 06:49:28 +0000

    practice let,var,const

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,18 @@
-console.log("Sagar joshi")
+const accountId=12345
+let accountEmail="sagar@gmail.com"
+var accountCity="kathmandu"
+accountState="bagmati"
+let name;
+
+// accountId=234567 //not allowed to change
+accountEmail="dipesh@gmail.com"
+accountCity="dhangadhi"
+
+/*
+prefer not to use var
+because of issue in blocked and functional scope
+
+*/
+
+
+console.table([accountId,accountEmail,accountCity,accountState,name])
