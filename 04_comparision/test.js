7b334ad2ae79073c40f025edfc81e78c22f2d8e4
Sagar Joshi <sagarjo637@gmail.com>
2026-01-03 17:00:34 +0000

    practice comparision

---
+++ 04_comparision/test.js
@@ -0,0 +1,27 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2<=1)
+// console.log(2!=1)
+
+
+/*
+JavaScript rule (important)
+When you use comparison operators (> < >= <=):
+ JavaScript converts strings to numbers (if possible)
+*/
+
+// console.log("2" >2); "2" → converted to number 2 ,Comparison becomes:2>2 which is false
+// console.log("02" >2);     "02" → converted to number 2 ,Comparison becomes:2>2 which is false
+
+
+/*
+Memory trick (one line)
+
+String vs String → alphabet comparison
+Number involved → string converts to number
+*/
+
+
+
+
