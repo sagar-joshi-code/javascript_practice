b2b4b5f61e89dea5b6cd5f35e27ef6da3c5a083b
Sagar Joshi <sagarjo637@gmail.com>
2026-01-04 10:13:02 +0000

    datatypes summary

---
+++ 04_comparision/datatypes_summary.js
@@ -0,0 +1,38 @@
+//primitive datatypes
+
+//7 types:string,number,boolean,null,undefined,symbol,bigint
+
+// const number=100
+// const score=100.55
+// isLoggedIn=true
+// const outSideTemperature=null
+// let userEmail;
+
+// let id=Symbol('abc')
+// let anotherId=Symbol('abc')
+// console.log(id === anotherId);
+// const bigNumber=1234567912345678n
+
+
+
+
+
+
+//Reference (non-primitive):Array,object,function
+
+let heroes=["shaktiman","spiderman","batman"]
+let myObj={
+    name:"sagar",
+    age:20
+}
+
+let myFunction=function(){
+    console.log("hello world");
+    
+}
+console.log(typeof myFunction);
+
+
+
+
+
