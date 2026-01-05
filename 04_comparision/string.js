355d3c23c6fff658b5296c54e37b022784ddd49e
Sagar Joshi <sagarjo637@gmail.com>
2026-01-05 09:36:59 +0000

    practice string

---
+++ 04_comparision/string.js
@@ -0,0 +1,49 @@
+// let name="sagar"
+// let repoCount=100
+// console.log(name +repoCount+"hello world");
+
+// let newValue=name.concat(repoCount)
+// console.log(newValue);
+
+
+
+// let val=[1,2,3,4,5]
+// val.shift()
+// console.log(val)
+
+// let firstName="sagar"
+// let lastName="joshi"
+// console.log(`My name is ${firstName} ${lastName}`)
+
+
+// let name="                        sagar     "
+// console.log(name);
+
+// console.log(name.trim());   //.trim removes whitespaces from both sides
+
+
+// let name =new String('sagar')
+// console.log(name.substring(0,4))    //we cannot use negative indexing
+
+
+// let name =new String('sagar')
+// console.log(name.slice(-4))    //we can use negative indexing
+
+
+// let name="sagar"
+// console.log(name.replace("s","_"))     //.replace(searchValue, newValue)
+
+let name="sagar"
+console.log(name.split(""))
+
+
+
+
+
+
+
+
+
+
+
+
