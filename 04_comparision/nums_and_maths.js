d05e5f7eb0778692045517943b0464f11ea60283
Sagar Joshi <sagarjo637@gmail.com>
2026-01-06 11:18:16 +0000

    practice number and maths

---
+++ 04_comparision/nums_and_maths.js
@@ -0,0 +1,26 @@
+// ***********************number*******************************
+
+// const score=500
+// console.log(score);
+
+// const balance=new Number(100)
+// console.log(balance.toFixed(2));
+
+
+// const otherNumber=11.123456
+// console.log(otherNumber.toPrecision(3));
+
+// *********************************maths******************************************
+
+// console.log(Math)
+// console.log(Math.abs(-3))      //.abs() converts negative value to positive value
+// console.log(Math.round(2.6))   //.round() rounds off value 
+// console.log(Math.ceil(2.01))   //Decimal vayo bhane mathi ko number linxa
+// console.log(Math.floor(2.9))   //Decimal vayo bhane tala ko number linxa
+// console.log(Math.min(2,3,4,5));
+// console.log(Math.max(2,3,4,5));
+
+console.log(Math.floor(Math.random()*10+1))
+
+
+
