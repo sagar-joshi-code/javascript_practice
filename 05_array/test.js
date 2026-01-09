5c6cebdd106e1902afcbe6022da9c78ef610248a
Sagar Joshi <sagarjo637@gmail.com>
2026-01-09 10:18:12 +0000

    practice array

---
+++ 05_array/test.js
@@ -0,0 +1,63 @@
+// let myArray=[1,2,3,4,5]
+// console.log(myArray.slice(1,3))   //in slice method of array there is no  change in original array but in splice there in change in original array
+// console.log(myArray);
+
+// let myNewArr= myArray.join()
+// console.log(myNewArr);
+// console.log(typeof myNewArr);
+
+
+// console.log(typeof myArray);
+
+
+// const marvelHeroes=["thor","ironman","spiderman",]
+// const dc_heroes=["superman","flashman","batman"]
+// const all_heroes=marvelHeroes.concat(dc_heroes)
+// console.log(all_heroes);
+
+
+
+//  const marvelHeroes=["thor","ironman","spiderman",]
+//   const dc_heroes=["superman","flashman","batman"]
+//   const all_heroes=[...marvelHeroes,...dc_heroes]
+//   console.log(all_heroes);
+  
+
+
+// const new_arr=[1,2,3,[4,5,6],7,[8,9,[10,11],12]]
+// const real_another_arr=new_arr.flat(3)
+// console.log(real_another_arr);
+
+
+// console.log(Array.isArray("sagar"));
+// console.log(Array.from("sagar"));
+
+
+
+// let score1=100
+// let score2=200
+// let score3=300
+// console.log(Array.of(score1,score2,score3));
+
+
+// 🧠 Easy comparison table
+
+// Method   	    What it does	              Example result
+// Array	        Constructor (confusing)	      avoid
+// Array.from()	    Convert to array	         "abc" → ["a","b","c"]
+// Array.of()	    Create array from values	  Array.of(5) → [5]
+
+
+// console.log(Array.from("sagar"));
+// let name="sagar"
+// console.log(Array.of(name));
+
+
+
+
+
+
+
+
+
+
