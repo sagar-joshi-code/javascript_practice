d3a28abc07000de1201dd006da70ee4596b54bf6
Sagar Joshi <sagarjo637@gmail.com>
2026-01-13 17:37:34 +0000

    practice  how to write normal function ,function expresssion and arrow function

---
+++ 07_function/test1.js
@@ -0,0 +1,78 @@
+//Normal function
+
+// function myName(){    //function definition or definition
+    // console.log("Sagar Joshi")
+// }
+// myName()     //function call
+
+
+//Function expression
+
+// const myName=function(){     //function expression
+//     console.log("Sagar Joshi")
+// }
+// myName()        //calling the function expression
+
+
+//Arrow function
+// const myName=()=>{    //Arrow function
+//     console.log("Sagar Joshi")
+// }
+// myName()           //calling the arrow function
+
+
+//normal function
+function normalFunction(){  //function definition or declaration
+    console.log("sagar Joshi")
+}
+
+//function expression
+const functionExpression=function(){    //function expression 
+     console.log("sagar Joshi")
+}
+
+//Arrow function
+const arrowFunction = () => {   //arrow function
+    console.log("sagar Joshi")
+
+}
+
+
+
+
+//Calling all three function
+// normalFunction()   //calling normal function 
+// functionExpression()  //calling function expression
+// arrowFunction()     //calling arrow function
+
+
+
+//mini practice 
+//normal function
+// printName()
+function printName(){
+    console.log("Sagar Joshi")
+}
+
+//function expression
+// showAge()
+const showAge=function(){
+    console.log(20)
+}
+
+//arrow function
+// showCity()
+const showCity= () => {
+    console.log("kathmandu")
+}
+
+
+// calling all three function
+printName()
+showAge()
+showCity()
+
+
+
+
+
